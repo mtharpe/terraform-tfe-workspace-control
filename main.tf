89844e9a771d450d464a6de802ba318d2985c2fd
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-01 22:26:14 -0400

    Update to ensure that I am creating the workspaces

--- main.tf
+++ main.tf
@@ -2,18 +2,12 @@ provider "tfe" {
   token    = var.test_rfc_tfc_api_key
 }
 
-# Disabled because of non-admin status
-# resource "tfe_organization" "main_org_setup" {
-#   name  = var.org_name
-#   email = "${var.org_name}@hashicorp.com"
-# }
-
-resource "tfe_workspace" "aws-demo_setup" {
-  name         = "basic-aws-demo"
+resource "tfe_workspace" "aws_demo_setup" {
+  name         = "aws_demo"
   organization = var.org_name
 }
 
-resource "tfe_workspace" "azure-demo_setup" {
-  name         = "basic-azure-demo"
+resource "tfe_workspace" "azure_demo_setup" {
+  name         = "azure_demo"
   organization = var.org_name
 }
