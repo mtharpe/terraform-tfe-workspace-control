143e19af1f7bf47ee67d05e651ceb3e5347583b5
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-01 22:27:27 -0400

    Update to format and workspace names

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "tfe" {
-  token    = var.test_rfc_tfc_api_key
+  token = var.test_rfc_tfc_api_key
 }
 
 resource "tfe_workspace" "aws_demo_setup" {
-  name         = "aws_demo"
+  name         = "aws-demo"
   organization = var.org_name
 }
 
 resource "tfe_workspace" "azure_demo_setup" {
-  name         = "azure_demo"
+  name         = "azure-demo"
   organization = var.org_name
 }
