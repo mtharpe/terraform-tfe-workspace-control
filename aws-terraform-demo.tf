10fffca26bc536e1589f2ff9fea2966d49c1b611
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-04 15:58:52 -0400

    Split Demos

---
+++ aws-terraform-demo.tf
@@ -0,0 +1,12 @@
+# AWS Demo
+
+resource "tfe_workspace" "aws_terraform_demo_setup" {
+  name             = "aws-terraform-demo"
+  organization     = var.org_name
+  queue_all_runs = false
+  vcs_repo {
+    identifier     = "mtharpe/terraform-aws-demo"
+    branch         = "master"
+    oauth_token_id = var.oauth_token_id
+  }
+}
--- main.tf
+++ main.tf
@@ -2,40 +2,6 @@ provider "tfe" {
   token = var.tfe_api_key
 }
 
-# Demos
-resource "tfe_workspace" "aws_terraform_demo_setup" {
-  name             = "aws-terraform-demo"
-  organization     = var.org_name
-  queue_all_runs = false
-  vcs_repo {
-    identifier     = "mtharpe/terraform-aws-demo"
-    branch         = "master"
-    oauth_token_id = var.oauth_token_id
-  }
-}
-
-resource "tfe_workspace" "azure_terraform_demo_setup" {
-  name         = "azure-terraform-demo"
-  organization = var.org_name
-  queue_all_runs = false
-  vcs_repo {
-    identifier     = "mtharpe/terraform-azure-demo"
-    branch         = "master"
-    oauth_token_id = var.oauth_token_id
-  }
-}
-
-resource "tfe_workspace" "gcp_terraform_demo_setup" {
-  name         = "gcp-terraform-demo"
-  organization = var.org_name
-  queue_all_runs = false
-  vcs_repo {
-    identifier     = "mtharpe/terraform-gcp-demo"
-    branch         = "master"
-    oauth_token_id = var.oauth_token_id
-  }
-}
-
 ## Consul
 // resource "tfe_workspace" "aws_consul_demo_setup" {
 //   name         = "aws-consul-demo"
