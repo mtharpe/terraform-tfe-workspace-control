e2993bf80a57e0aeff17f239fd53d775bdffd227
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-04 13:23:03 -0400

    Include lifecycles

--- main.tf
+++ main.tf
@@ -2,18 +2,56 @@ provider "tfe" {
   token = var.tfe_api_key
 }
 
-resource "tfe_workspace" "aws_demo_setup" {
-  name         = "aws-demo"
+# Prod
+resource "tfe_workspace" "prod_aws_demo_setup" {
+  name         = "prod-aws-demo"
   organization = var.org_name
 }
 
-resource "tfe_workspace" "azure_demo_setup" {
-  name         = "azure-demo"
+resource "tfe_workspace" "prod_azure_demo_setup" {
+  name         = "prod-azure-demo"
   organization = var.org_name
 }
 
-resource "tfe_workspace" "gcp_demo_setup" {
-  name         = "gcp-demo"
+# Dev
+resource "tfe_workspace" "dev_gcp_demo_setup" {
+  name         = "dev-gcp-demo"
+  organization = var.org_name
+}
+
+resource "tfe_workspace" "dev_aws_demo_setup" {
+  name         = "prod-aws-demo"
+  organization = var.org_name
+}
+
+resource "tfe_workspace" "dev_azure_demo_setup" {
+  name         = "dev-azure-demo"
+  organization = var.org_name
+}
+
+resource "tfe_workspace" "dev_gcp_demo_setup" {
+  name         = "dev-gcp-demo"
+  organization = var.org_name
+}
+
+# QA
+resource "tfe_workspace" "qa_gcp_demo_setup" {
+  name         = "qa-gcp-demo"
+  organization = var.org_name
+}
+
+resource "tfe_workspace" "qa_aws_demo_setup" {
+  name         = "qa-aws-demo"
+  organization = var.org_name
+}
+
+resource "tfe_workspace" "qa_azure_demo_setup" {
+  name         = "qa-azure-demo"
+  organization = var.org_name
+}
+
+resource "tfe_workspace" "qa_gcp_demo_setup" {
+  name         = "qa-gcp-demo"
   organization = var.org_name
 }
 
