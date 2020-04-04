65e7b5ad38166c76884065ef1be339bd18a3ad62
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-04 13:50:18 -0400

    Update to include VCS connections

--- main.tf
+++ main.tf
@@ -3,19 +3,34 @@ provider "tfe" {
 }
 
 # Demos
-resource "tfe_workspace" "gcp_terraform_demo_setup" {
-  name         = "gcp-terraform-demo"
-  organization = var.org_name
-}
-
 resource "tfe_workspace" "aws_terraform_demo_setup" {
-  name         = "aws-terraform-demo"
-  organization = var.org_name
+  name             = "aws-terraform-demo"
+  organization     = var.org_name
+  vcs_repo {
+    identifier     = "mtharpe/terraform-aws-demo"
+    branch         = "master"
+    oauth_token_id = var.oauth_token_id
+  }
 }
 
 resource "tfe_workspace" "azure_terraform_demo_setup" {
   name         = "azure-terraform-demo"
   organization = var.org_name
+  vcs_repo {
+    identifier     = "mtharpe/terraform-azure-demo"
+    branch         = "master"
+    oauth_token_id = var.oauth_token_id
+  }
+}
+
+resource "tfe_workspace" "gcp_terraform_demo_setup" {
+  name         = "gcp-terraform-demo"
+  organization = var.org_name
+  vcs_repo {
+    identifier     = "mtharpe/terraform-gcp-demo"
+    branch         = "master"
+    oauth_token_id = var.oauth_token_id
+  }
 }
 
 ## Consul
