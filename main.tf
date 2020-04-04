d8398f35a8a203dac41f14f0d51c1f048fe68630
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-04 13:30:38 -0400

    Update to remove tiers

--- main.tf
+++ main.tf
@@ -2,51 +2,19 @@ provider "tfe" {
   token = var.tfe_api_key
 }
 
-# Dev
-resource "tfe_workspace" "dev_gcp_demo_setup" {
-  name         = "dev-gcp-demo"
+# Demos
+resource "tfe_workspace" "gcp_demo_setup" {
+  name         = "gcp-demo"
   organization = var.org_name
 }
 
-resource "tfe_workspace" "dev_aws_demo_setup" {
-  name         = "dev-aws-demo"
+resource "tfe_workspace" "aws_demo_setup" {
+  name         = "aws-demo"
   organization = var.org_name
 }
 
-resource "tfe_workspace" "dev_azure_demo_setup" {
-  name         = "dev-azure-demo"
-  organization = var.org_name
-}
-
-# QA
-resource "tfe_workspace" "qa_gcp_demo_setup" {
-  name         = "qa-gcp-demo"
-  organization = var.org_name
-}
-
-resource "tfe_workspace" "qa_aws_demo_setup" {
-  name         = "qa-aws-demo"
-  organization = var.org_name
-}
-
-resource "tfe_workspace" "qa_azure_demo_setup" {
-  name         = "qa-azure-demo"
-  organization = var.org_name
-}
-
-# Prod
-resource "tfe_workspace" "prod_gcp_demo_setup" {
-  name         = "prod-gcp-demo"
-  organization = var.org_name
-}
-
-resource "tfe_workspace" "prod_aws_demo_setup" {
-  name         = "prod-aws-demo"
-  organization = var.org_name
-}
-
-resource "tfe_workspace" "prod_azure_demo_setup" {
-  name         = "prod-azure-demo"
+resource "tfe_workspace" "azure_demo_setup" {
+  name         = "azure-demo"
   organization = var.org_name
 }
 
