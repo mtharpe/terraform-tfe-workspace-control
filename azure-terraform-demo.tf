8e9e8ad42a12553b0c601c38662b5e09b2836efb
Michael Tharpe <mtharpe@hashicorp.com>
2020-04-04 15:58:52 -0400

    Split Demos

---
+++ azure-terraform-demo.tf
@@ -0,0 +1,12 @@
+# Azure Demo
+
+resource "tfe_workspace" "azure_terraform_demo_setup" {
+  name         = "azure-terraform-demo"
+  organization = var.org_name
+  queue_all_runs = false
+  vcs_repo {
+    identifier     = "mtharpe/terraform-azure-demo"
+    branch         = "master"
+    oauth_token_id = var.oauth_token_id
+  }
+}
---
+++ gcp-terraform-demo.tf
@@ -0,0 +1,12 @@
+# GCP Demo
+
+resource "tfe_workspace" "gcp_terraform_demo_setup" {
+  name         = "gcp-terraform-demo"
+  organization = var.org_name
+  queue_all_runs = false
+  vcs_repo {
+    identifier     = "mtharpe/terraform-gcp-demo"
+    branch         = "master"
+    oauth_token_id = var.oauth_token_id
+  }
+}
