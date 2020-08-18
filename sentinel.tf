02af1fc51d01f67269044a0c8e4cfb31fe6548ba
Michael Tharpe <mtharpe@hashicorp.com>
2020-08-18 11:35:48 -0400

    Update for lint

--- sentinel.tf
+++ sentinel.tf
@@ -7,9 +7,9 @@ resource "tfe_sentinel_policy" "base_policy" {
 }
 
 resource "tfe_policy_set" "base_policy_set" {
-  name                   = "base_policy_set"
-  description            = "This policy set holds base policy"
-  organization           = var.org_name
-  policy_ids             = ["${tfe_sentinel_policy.base_policy.id}"]
+  name          = "base_policy_set"
+  description   = "This policy set holds base policy"
+  organization  = var.org_name
+  policy_ids    = ["${tfe_sentinel_policy.base_policy.id}"]
   workspace_ids = ["${tfe_workspace.aws_terraform_demo_setup.id}", "${tfe_workspace.azure_terraform_demo_setup.id}", "${tfe_workspace.gcp_terraform_demo_setup.id}", ]
 }
