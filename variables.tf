d68c4f3e41cef2997d27387f4f4b2c2d1d9c24bd
Michael Tharpe <mtharpe@hashicorp.com>
2020-12-07 23:40:46 -0500

    Update to include session token for AWS

--- variables.tf
+++ variables.tf
@@ -18,6 +18,10 @@ variable "aws_secret_access_key" {
   description = "AWS Secret Access key"
 }
 
+variable "aws_session_token" {
+  description = "AWS Session Token"
+}
+
 variable "aws_region" {
   description = "AWS Region"
   default     = "us-east-2"
