477ce87d65cf5498b7c3086c5520c6de0493256f
Michael Tharpe <mtharpe@hashicorp.com>
2021-12-09 00:28:09 -0500

    Updated to add tags

--- azure-terraform-demo.tf
+++ azure-terraform-demo.tf
@@ -4,6 +4,7 @@ resource "tfe_workspace" "azure_terraform_demo_setup" {
   name           = "azure-terraform-demo"
   organization   = var.org_name
   queue_all_runs = false
+  tag_names    = ["demo"]
   vcs_repo {
     identifier     = "mtharpe/terraform-azure-demo"
     branch         = "master"
