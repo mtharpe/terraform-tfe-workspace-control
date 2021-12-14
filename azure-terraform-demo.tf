660ea392a08437bc1da25dc426faf2eead0b16b7
Michael Tharpe <mtharpe@hashicorp.com>
2021-12-13 22:12:01 -0500

    Update to support variable sets

--- azure-terraform-demo.tf
+++ azure-terraform-demo.tf
@@ -12,39 +12,6 @@ resource "tfe_workspace" "azure_terraform_demo_setup" {
   }
 }
 
-resource "tfe_variable" "azure_subscription_id" {
-  key          = "ARM_SUBSCRIPTION_ID"
-  value        = var.azure_subscription_id
-  category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup.id
-  description  = "Azure Subscription ID"
-}
-
-resource "tfe_variable" "azure_client_id" {
-  key          = "ARM_CLIENT_ID"
-  value        = var.azure_client_id
-  category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup.id
-  description  = "Azure Client ID"
-}
-
-resource "tfe_variable" "azure_tenant_id" {
-  key          = "ARM_TENANT_ID"
-  value        = var.azure_tenant_id
-  category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup.id
-  description  = "Azure Tenant ID"
-}
-
-resource "tfe_variable" "azure_client_secret" {
-  key          = "ARM_CLIENT_SECRET"
-  value        = var.azure_client_secret
-  category     = "env"
-  workspace_id = tfe_workspace.azure_terraform_demo_setup.id
-  description  = "Azure Client Secret"
-  sensitive    = true
-}
-
 resource "tfe_variable" "azure_instance_username" {
   key          = "azure_instance_username"
   value        = var.instance_username
--- variables.tf
+++ variables.tf
@@ -9,66 +9,10 @@ variable "oauth_token_id" {
   description = "Terraform Enterprise/Cloud VCS oauth token ID (found in settings/VCS)"
 }
 
-# AWS Variables
-variable "aws_access_key_id" {
-  description = "AWS Access key"
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access key"
-}
-
-variable "aws_session_token" {
-  description = "AWS Session Token"
-}
-
-variable "aws_region" {
-  description = "AWS Region"
-  default     = "us-east-2"
-}
-
-variable "public_key" {
-  description = "Public key to connect to instances"
-}
-
-variable "private_key" {
-  description = "Private key to connect to instances"
-}
-
 variable "instance_username" {
   description = "Username to connect to instances"
 }
 
 variable "instance_password" {
   description = "Password to connect to instances"
-}
-
-# GCP Variables
-variable "google_credentials" {
-  description = "Google Cloud Credentials"
-}
-
-variable "google_region" {
-  description = "Google Cloud Region"
-}
-
-variable "google_project" {
-  description = "Google Cloud Project"
-}
-
-# Azure Variables
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-}
-
-variable "azure_client_id" {
-  description = "Azure Client ID"
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-}
-
-variable "azure_client_secret" {
-  description = "Azure Clinet Secret"
 }
