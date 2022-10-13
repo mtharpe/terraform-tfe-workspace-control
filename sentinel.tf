46699f74c2bb19dd63a5d960dceda96298ed8386
Michael Tharpe <mtharpe@hashicorp.com>
2022-10-13 11:37:06 -0400

    Updated to just include AWS

--- sentinel.tf
+++ sentinel.tf
@@ -11,21 +11,3 @@ resource "tfe_policy_set" "demo_sentinel_policy_set" {
     oauth_token_id     = var.oauth_token_id
   }
 }
-
-## This is the old way, and can still be used but commenting out in favor of Sentinel Policy Sets
-#
-# resource "tfe_sentinel_policy" "base_policy" {
-#   name         = "base_policy"
-#   description  = "This policy always passes"
-#   organization = var.org_name
-#   policy       = "main = rule { true }"
-#   enforce_mode = "hard-mandatory"
-# }
-
-# resource "tfe_policy_set" "base_policy_set" {
-#   name          = "base_policy_set"
-#   description   = "This policy set holds base policy"
-#   organization  = var.org_name
-#   policy_ids    = [tfe_sentinel_policy.base_policy.id]
-#   workspace_ids = [tfe_workspace.aws_terraform_demo_setup.id, tfe_workspace.azure_terraform_demo_setup.id, tfe_workspace.gcp_terraform_demo_setup.id]
-# }
