d3d72222fa6842c2728185316092a15dda25e461
Michael Tharpe <mtharpe@gmail.com>
2023-04-19 15:37:39 -0400

    Updated for count.index

--- gcp-terraform-demo.tf
+++ gcp-terraform-demo.tf
@@ -1,7 +1,7 @@
 # GCP Demo
 
 resource "tfe_workspace" "gcp_terraform_demo_setup" {
-  count = var.create_gcp ? 1 : 0
+  count          = var.create_gcp ? 1 : 0
   name           = "gcp-terraform-demo"
   organization   = var.org_name
   queue_all_runs = false
@@ -14,20 +14,20 @@ resource "tfe_workspace" "gcp_terraform_demo_setup" {
 }
 
 resource "tfe_variable" "gcp_instance_username" {
-  count = var.create_gcp ? 1 : 0
+  count        = var.create_gcp ? 1 : 0
   key          = "gcp_instance_username"
   value        = var.instance_username
   category     = "terraform"
-  workspace_id = tfe_workspace.gcp_terraform_demo_setup.id[0]
+  workspace_id = tfe_workspace.gcp_terraform_demo_setup[count.index]
   description  = "Instance Username"
 }
 
 resource "tfe_variable" "gcp_instance_password" {
-  count = var.create_gcp ? 1 : 0
+  count        = var.create_gcp ? 1 : 0
   key          = "gcp_instance_password"
   value        = var.instance_password
   category     = "terraform"
-  workspace_id = tfe_workspace.gcp_terraform_demo_setup.id[0]
+  workspace_id = tfe_workspace.gcp_terraform_demo_setup[count.index]
   description  = "Instance Password"
   sensitive    = true
 }
