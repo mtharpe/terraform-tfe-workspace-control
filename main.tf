4aa4828b110ab253e1eaf30af8a62afb464b162c
Michael Tharpe <mtharpe@gmail.com>
2024-07-22 13:12:09 -0400

    Updated for tflint

--- main.tf
+++ main.tf
@@ -11,10 +11,3 @@ provider "tfe" {
   token = var.tfe_api_key
 }
 
-data "http" "tfc_ips" {
-  url = "https://app.terraform.io/api/meta/ip-ranges"
-
-  request_headers = {
-    Accept = "application/vnd.api+json"
-  }
-}
--- variables.tf
+++ variables.tf
@@ -1,37 +1,35 @@
 # Terraform Enterprise Variables
 variable "tfe_api_key" {
   description = "Terraform Enterprise/Cloud API used to connect and configure workspaces"
+  type        = string
 }
 variable "org_name" {
   description = "Terraform Enterprise/Cloud Organization name to create the workspaces in"
+  type        = string
 }
 variable "oauth_token_id" {
   description = "Terraform Enterprise/Cloud VCS oauth token ID (found in settings/VCS)"
-}
-
-# AWS Variables
-variable "aws_access_key_id" {
-  description = "Public key to connect to instances"
-}
-
-variable "aws_secret_access_key" {
-  description = "Private key to connect to instances"
+  type        = string
 }
 
 # SSH Variables
 variable "public_key" {
   description = "Public key to connect to instances"
+  type        = string
 }
 
 variable "private_key" {
   description = "Private key to connect to instances"
+  type        = string
 }
 
 # Windows Instance Account Info
 variable "instance_username" {
   description = "Username to connect to instances"
+  type        = string
 }
 
 variable "instance_password" {
   description = "Password to connect to instances"
+  type        = string
 }
