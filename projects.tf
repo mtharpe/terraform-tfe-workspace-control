121a910d5d1e7354fb04ea373c7f4e604981db1b
Michael Tharpe <mtharpe@gmail.com>
2024-07-22 19:40:59 -0400

    Updated projects to include description

--- projects.tf
+++ projects.tf
@@ -1,14 +1,17 @@
 resource "tfe_project" "aws" {
   organization = var.tfe_org_name
   name         = "AWS"
+  description  = "AWS Related Terraform Workspaces"
 }
 
 # resource "tfe_project" "gcp" {
 #   organization = var.tfe_org_name
 #   name         = "GCP"
+#   description  = "GCP Related Terraform Workspaces"
 # }
 
 # resource "tfe_project" "azure" {
 #   organization = var.tfe_org_name
 #   name         = "Azure"
+#   description  = "Azure Related Terraform Workspaces"
 # }
