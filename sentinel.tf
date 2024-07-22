a3d769967d36e1c3c60e83a8081cab39b195c1b6
Michael Tharpe <mtharpe@gmail.com>
2024-07-22 19:35:48 -0400

    Removed Sentinel for now until I can get a license from HC

--- sentinel.tf
+++ sentinel.tf
@@ -1,13 +1,13 @@
-resource "tfe_policy_set" "demo_sentinel_policy_set" {
-  name          = "demo-policy-set"
-  description   = "A brand new policy set for demos"
-  organization  = var.tfe_org_name
-  workspace_ids = [tfe_workspace.aws_terraform_demo_setup.id]
+# resource "tfe_policy_set" "demo_sentinel_policy_set" {
+#   name          = "demo-policy-set"
+#   description   = "A brand new policy set for demos"
+#   organization  = var.tfe_org_name
+#   workspace_ids = [tfe_workspace.aws_terraform_demo_setup.id]
 
-  vcs_repo {
-    identifier                 = "mtharpe/terraform-sentinel-common"
-    branch                     = "main"
-    ingress_submodules         = false
-    github_app_installation_id = var.github_app_installation_id
-  }
-}
+#   vcs_repo {
+#     identifier                 = "mtharpe/terraform-sentinel-common"
+#     branch                     = "main"
+#     ingress_submodules         = false
+#     github_app_installation_id = var.github_app_installation_id
+#   }
+# }
